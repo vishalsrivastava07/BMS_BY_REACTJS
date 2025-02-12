e3b9b5ec4187231f520985c02915f8719acb2d98
VishalSrivastava-007 <vishal.srivastava1@sourcefuse.com>
2025-02-12 10:00:58 +0530

    Added more test cases

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: ['./src/setupTests.ts'],  // Make sure this path matches your file location
+    setupFiles: ['./src/setupTests.ts'], 
   },
 })
